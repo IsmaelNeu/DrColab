0288117c5df814e94dba210bb1b10f6b2162b8d0
Ismael Neu <ismaelmmneu@hotmail.com>
2021-09-21 22:51:24 -0300

    Create DrColab.R
    Package description.

---
+++ R/DrColab.R
@@ -0,0 +1,23 @@
+###############################################################################
+###                                                                         ###
+###                           Script                                        ###
+###                       DESCRICAO PACKAGE                                 ###
+###+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++###
+###  Author: Ismael Neu                                                     ###
+###  Date: 21/09/2021                                                       ###
+###  Version: 1.0.1                                                         ###
+###  E-mail: ismaelmmneu@hotmail.com                                        ###
+###############################################################################
+#'
+#' @encoding UTF-8
+#' @title Análise Dr Colab
+#'
+#' @name DrColab
+#'
+#' @author Ismael Neu (ismaelmmneu@@hotmail.com)
+#'
+#' @description Pacote auxiliar.
+#'
+#' @docType package
+#'
+NULL
